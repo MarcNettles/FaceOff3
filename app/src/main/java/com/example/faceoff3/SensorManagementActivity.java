b4b6e920d31193e0e18830a97a27b067d5d93ad2
MarcNettles <mane9701@colorado.edu>
2020-11-12 15:19:21 -0700

    Switched to activity naming convention

--- app/src/main/java/com/example/faceoff3/SensorManagementActivity.java
+++ app/src/main/java/com/example/faceoff3/SensorManagementActivity.java
@@ -3,7 +3,7 @@ package com.example.faceoff3;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
 
-public class SensorManagement {
+public class SensorManagementActivity {
 
     private SensorManager sensorManager;
     private Sensor sensor;
@@ -14,6 +14,9 @@ public class SensorManagement {
 
     sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
     sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+
+
 
     
 }
